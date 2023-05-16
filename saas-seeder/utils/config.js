1d47d69e15944bf75141daf3706100ada380a8f5
vsakaria <vishal.sakaria@magnolia-cms.com>
2023-05-16 13:31:10 +0100

    UPDATE config

--- saas-seeder/utils/config.js
+++ saas-seeder/utils/config.js
@@ -1,9 +1,7 @@
-import StandardPage from '../templates/pages/StandardPage';
 import Home from '../templates/pages/Home';
 
 export const config = {
 	componentMappings: {
 		'insurance-demo:pages/Home': Home,
-		'insurance-demo:pages/StandardPage': StandardPage,
 	},
 };
