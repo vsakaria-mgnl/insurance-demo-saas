a3f3905518e0fba14027d1ba147f55b74ef661d5
vsakaria <vishal.sakaria@magnolia-cms.com>
2023-05-17 11:19:22 +0100

    UPDATE Banner remove JCR Types

--- saas-seeder/utils/config.js
+++ saas-seeder/utils/config.js
@@ -1,9 +1,13 @@
 import Home from '../templates/pages/Home';
 import Section from '../templates/components/Section';
+import Carousel from '../templates/components/Carousel';
+import Banner from '../templates/components/Banner';
 
 export const config = {
 	componentMappings: {
 		'insurance-demo:pages/Home': Home,
 		'insurance-demo:components/Section': Section,
+		'insurance-demo:components/Carousel': Carousel,
+		'insurance-demo:components/Banner': Banner,
 	},
 };
