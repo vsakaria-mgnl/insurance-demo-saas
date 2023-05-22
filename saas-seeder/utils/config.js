3e9545d2368ebf9a4ee16e69a2d9e18c1a18477b
vsakaria <vishal.sakaria@magnolia-cms.com>
2023-05-22 12:22:38 +0100

    ADD components

--- saas-seeder/utils/config.js
+++ saas-seeder/utils/config.js
@@ -2,6 +2,9 @@ import Home from '../templates/pages/Home';
 import Section from '../templates/components/Section';
 import Carousel from '../templates/components/Carousel';
 import Banner from '../templates/components/Banner';
+import TextImage from '../templates/components/TextImage';
+import Card from '../templates/components/Card';
+import Columns from '../templates/components/Columns';
 
 export const config = {
 	componentMappings: {
@@ -9,5 +12,8 @@ export const config = {
 		'insurance-demo:components/Section': Section,
 		'insurance-demo:components/Carousel': Carousel,
 		'insurance-demo:components/Banner': Banner,
+		'insurance-demo:components/TextImage': TextImage,
+		'insurance-demo:components/Card': Card,
+		'insurance-demo:components/Columns': Columns,
 	},
 };
