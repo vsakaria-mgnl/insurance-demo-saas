e76f8d508caa47e500af7b34108c97bb236d20a7
vsakaria <vishal.sakaria@magnolia-cms.com>
2023-05-22 12:44:08 +0100

    TEST bug

--- saas-seeder/templates/components/Columns.js
+++ saas-seeder/templates/components/Columns.js
@@ -1,19 +1,28 @@
 import { EditableArea } from '@magnolia/react-editor';
 
 function Columns(props) {
-  const { showCol1, col1, showCol2, col2, showCol3, col3, showCol4, col4 } = props;
-  let colWidth = 'col-' + 12 / [showCol1, showCol2, showCol3, showCol4].filter(Boolean).length;
+	const { showCol1, col1, showCol2, col2, showCol3, col3, showCol4, col4 } =
+		props;
 
-  return (
-    <div className='Columns'>
-      <div className='row'>
-        {showCol1 && col1 && <EditableArea className={colWidth} content={col1} />}
-        {showCol2 && col2 && <EditableArea className={colWidth} content={col2} />}
-        {showCol3 && col3 && <EditableArea className={colWidth} content={col3} />}
-        {showCol4 && col4 && <EditableArea className={colWidth} content={col4} />}
-      </div>
-    </div>
-  );
+	return (
+		<div className="Columns">
+			<div className="row">Hello</div>
+		</div>
+	);
 }
 
 export default Columns;
+
+/*
+
+let colWidth =
+		'col-' +
+		12 / [showCol1, showCol2, showCol3, showCol4].filter(Boolean).length;
+
+
+  {showCol1 && col1 && <EditableArea className={colWidth} content={col1} />}
+  {showCol2 && col2 && <EditableArea className={colWidth} content={col2} />}
+  {showCol3 && col3 && <EditableArea className={colWidth} content={col3} />}
+  {showCol4 && col4 && <EditableArea className={colWidth} content={col4} />}
+
+*/
