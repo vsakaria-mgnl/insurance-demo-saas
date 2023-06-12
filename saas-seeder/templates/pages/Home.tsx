62d43a67ced31d9fb0b9e402d7507ef70c17fcdf
vsakaria <vishal.sakaria@magnolia-cms.com>
2023-06-12 11:52:06 +0100

    ADD Page

--- saas-seeder/templates/pages/Home.tsx
+++ saas-seeder/templates/pages/Home.tsx
@@ -5,7 +5,6 @@ const url =
 	'https://author.sales-demo-prod.eu-playground.magnolia-platform.com/dam/jcr:8b9597ab-847e-4b0e-8152-7ad9cd10a8a1/Background_bottom_01.png';
 
 function renderHomeHeader(content: any) {
-	console.log(content);
 	return (
 		<div className="header">
 			Header
@@ -25,7 +24,6 @@ export default function Home(props: any) {
 
 	return (
 		<>
-			<h1>I am </h1>
 			<main style={{ backgroundImage: 'url(' + url + ')' }}>
 				{header && (
 					<EditableArea
