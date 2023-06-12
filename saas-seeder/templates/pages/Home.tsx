e9b988f25e20d09dc8dd83294f179aa85a322d37
vsakaria <vishal.sakaria@magnolia-cms.com>
2023-06-12 16:17:09 +0100

    ADD

--- saas-seeder/templates/pages/Home.tsx
+++ saas-seeder/templates/pages/Home.tsx
@@ -1,30 +1,17 @@
 import React from 'react';
 import { EditableArea, EditableComponent } from '@magnolia/react-editor';
 
-const url =
-	'https://author.sales-demo-prod.eu-playground.magnolia-platform.com/dam/jcr:8b9597ab-847e-4b0e-8152-7ad9cd10a8a1/Background_bottom_01.png';
-
-function renderHomeHeader(content: any) {
-	return (
-		<div className="header">
-			Header
-			{/* {content['@nodes'].map((nodeName: any) => (
-				<div key={content[nodeName]['@id']}>
-					<EditableComponent content={{ ...content[nodeName] }} />
-				</div>
-			))} */}
-		</div>
-	);
+function renderHomeHeader({ content }: any) {
+	console.table('content', content);
+	return <div className="header">Header</div>;
 }
 
 export default function Home(props: any) {
 	const { main, header } = props;
 
-	console.log(props);
-
 	return (
 		<>
-			<main style={{ backgroundImage: 'url(' + url + ')' }}>
+			<main>
 				{header && (
 					<EditableArea
 						content={header}
