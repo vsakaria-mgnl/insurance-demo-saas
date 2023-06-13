24d8af1e68369c027fc98f8ce28d503e5c403f59
vsakaria <vishal.sakaria@magnolia-cms.com>
2023-06-13 09:31:04 +0100

    UPDATE A component

--- saas-seeder/components/A.js
+++ saas-seeder/components/A.js
@@ -4,27 +4,9 @@ import Link from 'next/link';
 // import MagnoliaContext from '../utils/MagnoliaContext';
 
 function A(props) {
-	const { className, href, label, children, searchParams } = props;
-	// const magnoliaContext = useContext(MagnoliaContext);
-	const isFallbackLocale = magnoliaContext.currentLanguage === 'en';
-	let pathname = href.replace(
-		new RegExp('(.*' + 'spaRootNodePath' + '|.html|/$)', 'g'),
-		''
-	);
+	const { href, label } = props;
 
-	return (
-		<Link
-			href={{
-				pathname,
-				query: Object.assign({}, searchParams),
-			}}
-		>
-			<a className={className}>
-				{label}
-				{children}
-			</a>
-		</Link>
-	);
+	return <Link href={href}>{label}</Link>;
 }
 
 export default A;
